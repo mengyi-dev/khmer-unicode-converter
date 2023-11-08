ea76fd48513efd81134a7be260e718725c0143a4
Mengyi YOENG <90229183+mengyi-dev@users.noreply.github.com>
2023-11-08 15:12:07 +0700

    test google adsence

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  head: {
+    script: [
+      {
+        src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4357194654851697",
+        crossorigin: "anonymous",
+        async: true
+      }
+    ]
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
