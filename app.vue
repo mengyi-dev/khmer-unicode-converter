455b584ee03aeaf2233d77cf3b9bc0a4754da4ae
Mengyi YOENG <90229183+mengyi-dev@users.noreply.github.com>
2023-12-14 10:52:39 +0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
     <div>
         <Head>
           <Title>Limon F1 | Converter | Khmer fonts — ​ពុម្ព​អក្សរ​ខ្មែរ | Capcut</Title>
-          <meta name="description" content="This is a great webiste site to handle font Khmer error on Capcut. What you need is just paste and copy.">
+          <meta hid="description" name="description" content="This is a great webiste site to handle font Khmer error on Capcut. What you need is just paste and copy.">
           <meta name="keywords" content="khmer text converter, unicode khmer converter, online khmer unicode converter, free khmer unicode converter, khmer script to unicode converter, convert khmer font to unicode, transliterate khmer to unicode, khmer unicode encoding, unicode khmer typography, digital khmer documents, khmer legacy font converter, limon font converter, khmer pronunciation guide, khmer dictionary integration, copy-paste khmer unicode, khmer unicode keyboard layout, khmer document translation, batch khmer unicode conversion, custom khmer unicode settings, បម្លែងអក្សរខ្មែរទៅយូនីកូដ, អក្សរខ្មែរតួអក្សរយូនីកូដ, ស្គិតស្ទូរខ្មែរយូនីកូដ">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <meta name="google-adsense-account" content="ca-pub-4357194654851697">
