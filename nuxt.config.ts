b8a470e8be42d9c005e419326c45abd80fb6f4ab
Mengyi YOENG <90229183+mengyi-dev@users.noreply.github.com>
2023-12-14 10:40:19 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     head: {
-      meta: {
-      title: 'Limon F1 | Converter | Khmer fonts — ​ពុម្ព​អក្សរ​ខ្មែរ | Capcut',
-      description: 'This is a great webiste site to handle font Khmer error on Capcut. What you need is just paste and copy',
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1.0',
-    },
+    title: 'Limon F1 | Converter | Khmer fonts — ​ពុម្ព​អក្សរ​ខ្មែរ | Capcut',
+    meta: [
+      { name: 'description', content: 'This is a great webiste site to handle font Khmer error on Capcut. What you need is just paste and copy.' },
+      { name: 'keywords', content: 'khmer text converter, unicode khmer converter, online khmer unicode converter, free khmer unicode converter, khmer script to unicode converter, convert khmer font to unicode, transliterate khmer to unicode, khmer unicode encoding, unicode khmer typography, digital khmer documents, khmer legacy font converter, limon font converter, khmer pronunciation guide, khmer dictionary integration, copy-paste khmer unicode, khmer unicode keyboard layout, khmer document translation, batch khmer unicode conversion, custom khmer unicode settings, បម្លែងអក្សរខ្មែរទៅយូនីកូដ, អក្សរខ្មែរតួអក្សរយូនីកូដ, ស្គិតស្ទូរខ្មែរយូនីកូដ' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+      { name: 'google-adsense-account', content: 'ca-pub-4357194654851697' },
+    ],
     script: [
       {
         src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4357194654851697",
