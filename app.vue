8da8d0d73a685d7bad5cd47f36c4282c0b351805
Mengyi YOENG <90229183+mengyi-dev@users.noreply.github.com>
2023-12-14 10:28:15 +0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -8,7 +8,7 @@
         </Head>
         <div class="mx-auto w-full p-5">
             <div class="mx-auto w-full md:w-9/12">
-                <img src="@/assets/img/logo.png" width="300">
+                <img src="@/assets/img/logo.png" width="300" alt="logo">
                 <InputForm />
             </div>
         </div>
