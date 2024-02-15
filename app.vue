c6139a02a360e260dde9d1cb44f324198b7c3d89
Mengyi YOENG <90229183+mengyi-dev@users.noreply.github.com>
2024-02-15 15:58:58 +0000

    add payment link

--- app.vue
+++ app.vue
@@ -1,29 +1,44 @@
 <template>
-    <div>
-        <Head>
-          <Title>Limon F1 | Converter | Khmer fonts — ​ពុម្ព​អក្សរ​ខ្មែរ | Capcut</Title>
-          <meta hid="description" name="description" content="This is a great webiste site to handle font Khmer error on Capcut. What you need is just paste and copy.">
-          <meta name="keywords" content="khmer text converter, unicode khmer converter, online khmer unicode converter, free khmer unicode converter, khmer script to unicode converter, convert khmer font to unicode, transliterate khmer to unicode, khmer unicode encoding, unicode khmer typography, digital khmer documents, khmer legacy font converter, limon font converter, khmer pronunciation guide, khmer dictionary integration, copy-paste khmer unicode, khmer unicode keyboard layout, khmer document translation, batch khmer unicode conversion, custom khmer unicode settings, បម្លែងអក្សរខ្មែរទៅយូនីកូដ, អក្សរខ្មែរតួអក្សរយូនីកូដ, ស្គិតស្ទូរខ្មែរយូនីកូដ">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <meta name="google-adsense-account" content="ca-pub-4357194654851697">
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4357194654851697"
-     crossorigin="anonymous"></script>
-        </Head>
-        <div class="mx-auto w-full p-5">
-            <div class="mx-auto w-full md:w-9/12">
-                <img src="@/assets/img/logo.png" width="300" alt="logo">
-                <InputForm />
-                <p class="mt-10 mb-2">ចូលទៅទាញយក Limon Font 👇</p>
-                <a href="https://khmersoft.net/download-font-limon-khmer-font-for-your-computer/" target="_blank">
-                  <button class="rounded-lg px-4 py-2 bg-blue-500 text-blue-100 hover:bg-blue-600 duration-300">Download Limon Font</button>
-                </a>
-            </div>
+  <div>
+    <Head>
+      <Title>Limon F1 | Converter | Khmer fonts — ​ពុម្ព​អក្សរ​ខ្មែរ | Capcut</Title>
+      <meta hid="description" name="description" content="This is a great webiste site to handle font Khmer error on Capcut. What you need is just paste and copy.">
+      <meta name="keywords" content="khmer text converter, unicode khmer converter, online khmer unicode converter, free khmer unicode converter, khmer script to unicode converter, convert khmer font to unicode, transliterate khmer to unicode, khmer unicode encoding, unicode khmer typography, digital khmer documents, khmer legacy font converter, limon font converter, khmer pronunciation guide, khmer dictionary integration, copy-paste khmer unicode, khmer unicode keyboard layout, khmer document translation, batch khmer unicode conversion, custom khmer unicode settings, បម្លែងអក្សរខ្មែរទៅយូនីកូដ, អក្សរខ្មែរតួអក្សរយូនីកូដ, ស្គិតស្ទូរខ្មែរយូនីកូដ">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="google-adsense-account" content="ca-pub-4357194654851697">
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4357194654851697"
+  crossorigin="anonymous"></script>
+    </Head>
+    <div class="mx-auto w-full p-5">
+      <div class="mx-auto w-full md:w-9/12">
+        <img src="@/assets/img/logo.png" width="300" alt="logo">
+        <InputForm />
+        <div class="lg:flex gap-x-10"> 
+          <div>
+            <p class="mt-10 mb-2">ចូលទៅទាញយក Limon Font 👇</p>
+            <a href="https://khmersoft.net/download-font-limon-khmer-font-for-your-computer/" target="_blank">
+              <button class="rounded-lg px-4 py-2 bg-blue-500 text-blue-100 hover:bg-blue-600 duration-300">Download Limon Font</button>
+            </a>
+          </div>
+          <div>
+            <p class="mt-10 mb-2">ប៉ាវ កាហ្វេ😁</p>
+            <a href="https://link.payway.com.kh/aba?id=DE6AAD8E7E3C&code=360598&acc=500050202&dynamic=true" target="_blank">
+              <button @open="openDailog = true" class="rounded-lg px-4 py-2 bg-blue-500 text-blue-100 hover:bg-blue-600 duration-300">Pay now</button>
+            </a>
+          </div>
         </div>
+      </div>
     </div>
-    
+  </div>
+  <!-- <PaymentDailog :value="openDailog" @close="openDailog = false" @open="openDailog = true"/> -->
 </template>
 <script>
 export default {
+  data() {
+    return {
+      openDailog: false
+    }
+  },
     head () {
         return {
           title: 'Adsence',
---
+++ components/PaymentDailog.vue
@@ -0,0 +1,84 @@
+<template>
+  <!-- <div class="fixed inset-1 flex items-center justify-center">
+    <button
+      type="button"
+      @click="openModal"
+      class="rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+    >
+      Open dialog
+    </button>
+  </div> -->
+  <TransitionRoot appear :show="props.value" as="template">
+    <Dialog as="div" @close="closeModal" class="relative">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-1 bg-black/25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full max-w-[250px] transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+            >
+              <div class="mt-2">
+                <img src="@/assets/img/qr.jpg" alt="aba_qr" width="250">
+              </div>
+
+              <div class="mt-4">
+                <button
+                  type="button"
+                  class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                  @click="closeModal"
+                >
+                  No, thanks!
+                </button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
+
+<script setup>
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from '@headlessui/vue'
+
+const props = defineProps({
+  value: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+function closeModal() {
+  this.$emit('close')
+}
+function openModal() {
+  this.$emit('open')
+}
+</script>
