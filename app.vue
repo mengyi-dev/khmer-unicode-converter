4f21f0efa597a66d4cda934170f96986dc5a7846
Mengyi YOENG <90229183+mengyi-dev@users.noreply.github.com>
2025-04-04 01:00:23 +0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -5,9 +5,6 @@
       <meta hid="description" name="description" content="This is a great webiste site to handle font Khmer error on Capcut. What you need is just paste and copy.">
       <meta name="keywords" content="khmer text converter, unicode khmer converter, online khmer unicode converter, free khmer unicode converter, khmer script to unicode converter, convert khmer font to unicode, transliterate khmer to unicode, khmer unicode encoding, unicode khmer typography, digital khmer documents, khmer legacy font converter, limon font converter, khmer pronunciation guide, khmer dictionary integration, copy-paste khmer unicode, khmer unicode keyboard layout, khmer document translation, batch khmer unicode conversion, custom khmer unicode settings, បម្លែងអក្សរខ្មែរទៅយូនីកូដ, អក្សរខ្មែរតួអក្សរយូនីកូដ, ស្គិតស្ទូរខ្មែរយូនីកូដ">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta name="google-adsense-account" content="ca-pub-4357194654851697">
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4357194654851697"
-  crossorigin="anonymous"></script>
     </Head>
     <div class="mx-auto w-full p-5">
       <div class="mx-auto w-full md:w-9/12">
@@ -38,19 +35,6 @@ export default {
     return {
       openDailog: false
     }
-  },
-    head () {
-        return {
-          title: 'Adsence',
-          script: [
-            {
-              hid: 'adsence',
-              src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4357194654851697',
-              crossorigin: "anonymous",
-              async: true
-            }
-          ]
-        }
-      }
+  }
 }
 </script>
